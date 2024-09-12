304e4873f4d559376e83c07874e983f402e740fb
Gibran-Ca <135308799+Gibran-Ca@users.noreply.github.com>
2024-09-12 02:34:32 -0600

    Add files via upload
    Factory Method

---
+++ conexion/db_connector/mysql_factory.py
@@ -0,0 +1,12 @@
+
+from db_connector.factory import ConnectionFactory
+from db_connector.mysql_connection import MySQLConnection
+
+class MySQLConnectionFactory(ConnectionFactory):
+    def create_connection(self) -> MySQLConnection:
+        return MySQLConnection(
+            host='localhost',
+            user='root',
+            password='',
+            db='prueba'
+        )
